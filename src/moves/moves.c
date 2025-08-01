9aae9ae83ca5227e0a924ba2f8092d801be5f282
pnurmi <pnurmi.zt@gmail.com>
2025-08-01 11:38:52 +0300

    progress made on operations

--- src/moves/moves.c
+++ src/moves/moves.c
@@ -6,7 +6,7 @@
 /*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 09:37:46 by pnurmi            #+#    #+#             */
-/*   Updated: 2025/08/01 09:51:39 by pnurmi           ###   ########.fr       */
+/*   Updated: 2025/08/01 11:38:22 by pnurmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	move_sa(t_node **stack_a)
 	second->next = first;
 	first->prev = second;
 	*stack_a = second;
+	ft_putstr_fd("sa\n", 1);
 }
 
 void	move_sb(t_node **stack_b)
@@ -38,9 +39,44 @@ void	move_sb(t_node **stack_b)
 	if (!stack_b || !*stack_b || !(*stack_b)->next)
 		return ;
 	first = *stack_b;
+	second = first->next;
+	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	*stack_b = second;
+	ft_putstr_fd("sb\n", 1);
 }
-void	move_ss(t_node stack_a, t_node stack_b);
-void	move_pa(t_node stack_a, t_node stack_b);
+
+void	move_ss(t_node **stack_a, t_node **stack_b)
+{
+	move_sa(stack_a);
+	move_sb(stack_b);
+	ft_putstr_fd("ss\n", 1);
+}
+
+void	move_pa(t_node **stack_a, t_node **stack_b)
+{
+	t_node	*first_a;
+	t_node	*first_b;
+
+	if (!stack_b || !*stack_b)
+		return ;
+	first_a = *stack_a;
+	first_b = *stack_b;
+	if (*stack_b)
+		(*stack_b)->prev = NULL;
+	first_b->prev = NULL;
+	if (*stack_a)
+		(*stack_a)->prev = first_b;
+	first_a->prev = first_b;
+	first_b->next = first_a;
+	*stack_a = first_b;
+	ft_putstr_fd("pa\n", 1);
+}
+
 void	move_pb(t_node stack_a, t_node stack_b);
 void	move_ra(t_node stack_a, t_node stack_b);
 void	move_rb(t_node stack_a, t_node stack_b);
