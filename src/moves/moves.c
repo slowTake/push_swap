257f2a5dbeed192aadf188a6dd4b54ca110b43aa
pnurmi <pnurmi.zt@gmail.com>
2025-08-01 11:52:33 +0300

    push operations ready

--- src/moves/moves.c
+++ src/moves/moves.c
@@ -6,7 +6,7 @@
 /*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 09:37:46 by pnurmi            #+#    #+#             */
-/*   Updated: 2025/08/01 11:38:22 by pnurmi           ###   ########.fr       */
+/*   Updated: 2025/08/01 11:52:20 by pnurmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,25 +59,39 @@ void	move_ss(t_node **stack_a, t_node **stack_b)
 
 void	move_pa(t_node **stack_a, t_node **stack_b)
 {
-	t_node	*first_a;
 	t_node	*first_b;
 
 	if (!stack_b || !*stack_b)
 		return ;
-	first_a = *stack_a;
 	first_b = *stack_b;
+	*stack_b = (*stack_b)->next;
 	if (*stack_b)
 		(*stack_b)->prev = NULL;
 	first_b->prev = NULL;
+	first_b->next = *stack_a;
 	if (*stack_a)
 		(*stack_a)->prev = first_b;
-	first_a->prev = first_b;
-	first_b->next = first_a;
 	*stack_a = first_b;
 	ft_putstr_fd("pa\n", 1);
 }
 
-void	move_pb(t_node stack_a, t_node stack_b);
+void	move_pb(t_node **stack_a, t_node **stack_b)
+{
+	t_node	*first_a;
+
+	if (!stack_a || !*stack_a)
+		return ;
+	first_a = *stack_a;
+	*stack_a = (*stack_a)->next;
+	if (*stack_a)
+		(*stack_a)->prev = NULL;
+	first_a->prev = NULL;
+	first_a->next = *stack_b;
+	if (*stack_b)
+		(*stack_b)->prev = first_a;
+	*stack_b = first_a;
+	ft_putstr_fd("pb\n", 1);
+}
 void	move_ra(t_node stack_a, t_node stack_b);
 void	move_rb(t_node stack_a, t_node stack_b);
 void	move_rr(t_node stack_a, t_node stack_b);
