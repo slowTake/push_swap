21fd26400ee119e1a2eaed8e4de035bbaea9aa40
pnurmi <pnurmi.zt@gmail.com>
2025-08-06 16:07:42 +0300

    pushing but with errors

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 11:51:09 by pnurmi            #+#    #+#             */
-/*   Updated: 2025/08/06 13:15:00 by pnurmi           ###   ########.fr       */
+/*   Updated: 2025/08/06 16:07:28 by pnurmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 int	main(int argc, char *argv[])
 {
 	t_node	*stack_a;
+	t_node	*stack_b;
 	int		*sorted_stack;
 	int		i;
+	int		size;
 
+	stack_a = NULL;
+	stack_b = NULL;
 	i = 1;
+	if (argc < 2)
+		return (0);
 	while (i < argc)
 	{
 		if (!check_arg(argv[i], argc))
@@ -26,19 +32,27 @@ int	main(int argc, char *argv[])
 		i++;
 	}
 	stack_a = parse_arg(argc, argv);
+	if (check_sorted(stack_a))
+	{
+		ft_clearlist(&stack_a);
+		return (0);
+	}
+	size = ft_listsize(stack_a);
+	if (size <= 1)
+	{
+		ft_clearlist(&stack_a);
+		return (0);
+	}
 	sorted_stack = stack_to_array(&stack_a, ft_listsize(stack_a));
+	if (!sorted_stack)
+	{
+		ft_clearlist(&stack_a);
+		return (1);
+	}
 	bubble_sort(sorted_stack, ft_listsize(stack_a));
 	normalize_stack(&stack_a, sorted_stack, ft_listsize(stack_a));
+	radix_sort(&stack_a, &stack_b);
 	free(sorted_stack);
-	if (check_sorted(stack_a))
-		ft_printf("sorted!");
 	ft_clearlist(&stack_a);
+	ft_clearlist(&stack_b);
 }
-
-// value tester and checker: while, tmp
-// tmp = stack_a;
-// while (tmp)
-// {
-// 	printf("%d\n", tmp->value);
-// 	tmp = tmp->next;
-// }
